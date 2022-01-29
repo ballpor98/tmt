9855d719fd12de18a0664eef735f3ce336286142
Chawin Kasikitpongpan <chawin.k@kbtg.tech>
2022-01-29 23:28:06 +0700

    refactor: restructure project name

--- tmt/wsgi.py
+++ tmt/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for apps project.
+WSGI config for tmt project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apps.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tmt.settings')
 
 application = get_wsgi_application()
