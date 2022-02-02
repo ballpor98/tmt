68aa2b9261f38905dd0841f8f3ecce9860ad83b9
Chawin Kasikitpongpan <chawin.k@kbtg.tech>
2022-02-02 17:34:10 +0700

    feat: update admin page

--- tmt/clocking/admin.py
+++ tmt/clocking/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from tmt.clocking.models import User
+from tmt.clocking.models import User, Clock, CurrentClock
+
+
+class ClockAdmin(admin.ModelAdmin):
+    list_display = ('user', 'clocked_in', 'clocked_out')
+
+
+class CurrentClockAdmin(admin.ModelAdmin):
+    list_display = ('user', 'clock')
+
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Clock, ClockAdmin)
+admin.site.register(CurrentClock, CurrentClockAdmin)
