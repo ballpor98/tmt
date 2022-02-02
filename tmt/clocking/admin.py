c4e0da6893a75e395f55615b082426df0f79eb67
Chawin Kasikitpongpan <chawin.k@kbtg.tech>
2022-02-02 11:30:45 +0700

    chore: change admin more simple code

--- tmt/clocking/admin.py
+++ tmt/clocking/admin.py
@@ -1,18 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import User
 from tmt.clocking.models import Profile
 
-
-class AdminProfileInline(admin.StackedInline):
-    model = Profile
-    can_delete = False
-    verbose_name_plural = 'profiles'
-
-
-class ProfileUserAdmin(BaseUserAdmin):
-    inlines = (AdminProfileInline,)
-
-
-admin.site.unregister(User)
-admin.site.register(User, ProfileUserAdmin)
+admin.site.register(Profile)
