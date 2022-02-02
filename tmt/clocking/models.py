50463d50757e5a8fe70bbef0dc18730e1636ea26
Chawin Kasikitpongpan <chawin.k@kbtg.tech>
2022-02-02 17:33:23 +0700

    feat: add current clock table

--- tmt/clocking/models.py
+++ tmt/clocking/models.py
@@ -10,7 +10,15 @@ class User(AbstractUser):
 class Clock(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='clocking_logs', on_delete=models.CASCADE)
     clocked_in = models.DateTimeField(auto_now_add=True)
-    clocked_out = models.DateTimeField()
+    clocked_out = models.DateTimeField(null=True)
 
     def __str__(self):
-        return self.name
+        return f'{self.user}: {self.clocked_in} - {self.clocked_out}'
+
+
+class CurrentClock(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, primary_key=True, on_delete=models.CASCADE)
+    clock = models.OneToOneField(Clock, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user}: {self.clock}'
