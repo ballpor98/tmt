382df3e258126ed6d32d4f688355488e9a97c4c7
Chawin Kasikitpongpan <chawin.k@kbtg.tech>
2022-02-02 16:19:14 +0700

    refactor: lower user and clock coupling

--- tmt/clocking/models.py
+++ tmt/clocking/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -7,7 +8,7 @@ class User(AbstractUser):
 
 
 class Clock(models.Model):
-    user = models.ForeignKey(User, related_name='clocking_logs', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='clocking_logs', on_delete=models.CASCADE)
     clocked_in = models.DateTimeField(auto_now_add=True)
     clocked_out = models.DateTimeField()
 
