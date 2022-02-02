9142d83c0193b39c9b1da1b67b4ec0171485a97a
Chawin Kasikitpongpan <chawin.k@kbtg.tech>
2022-02-02 18:04:56 +0700

    feat: add currentClock query

--- tmt/clocking/schema/query.py
+++ tmt/clocking/schema/query.py
@@ -2,7 +2,7 @@ import graphene
 from django.contrib.auth import get_user_model
 from graphene_django import DjangoObjectType
 
-from tmt.clocking.models import Clock
+from tmt.clocking.models import Clock, ActiveClocking
 
 
 class ClockType(DjangoObjectType):
@@ -17,11 +17,26 @@ class UserType(DjangoObjectType):
         fields = ("id", "username", "email")
 
 
+class CurrentClock(DjangoObjectType):
+    class Meta:
+        model = ActiveClocking
+        fields = ("clock",)
+
+
 class Query(graphene.ObjectType):
     me = graphene.Field(UserType)
+    current_clock = graphene.Field(CurrentClock)
 
     def resolve_me(self, info):
         user = info.context.user
         if user.is_anonymous:
             raise Exception('Authentication Failure: Your must be signed in')
         return user
+
+    def resolve_current_clock(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('Authentication Failure: Your must be signed in')
+        if not hasattr(user, 'current_clock'):
+            raise Exception('Query currentClock Failure: Not have any currentClock now')
+        return user.current_clock
